b93c41db1a3041e5e73a905926899237da87a904
Cedric <cedric.luckie@gmail.com>
2016-01-19 17:09:09 -0500

    Mejoras al módulo del cima

--- www/scripts/shared/controllers.js
+++ www/scripts/shared/controllers.js
@@ -7,6 +7,12 @@
         vm.loading = true;
         Cozumel.clima()
           .success(function (data) {
+            vm.detalles = data.query.results.channel.item;
+            vm.temperatura = {};
+            console.log(vm.detalles);
+            vm.temperatura.f = vm.detalles.condition.temp;
+            vm.temperatura.c = parseInt((vm.temperatura.f - 32) * (5/9));
+            vm.temperatura.code = vm.detalles.condition.code;
             vm.clima = data.query.results.channel.item.description;
             vm.loading = false;
           })
